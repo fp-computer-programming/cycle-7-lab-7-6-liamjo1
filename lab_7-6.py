8eead69dfbfcc84054adab558aba2f6a301027f4
p24lohara <p24lohara@fairfieldprep.org>
2023-12-11 17:24:35 -0500

    lab 7-6 submission

--- lab_7-6.py
+++ lab_7-6.py
@@ -7,3 +7,15 @@ Create a 2 separate functions within the same document.
 Create a 3rd function which requires both the first two functions within it
 Create 4 test cases for your 3rd function. 
 """
+# Liam O'Hara
+
+def grams_to_kg(grams):
+    return gram * 1000
+def m_times_m(m):
+    return m*m
+
+# Create a 3rd function which requires both the first two functions within it
+
+def bmi (grams,m):
+    return grams_to_kg(grams)/m_times_m(m)
+# Create 4 test cases for your 3rd function. 
